fbb3b0fc665f6ba7388c6f97fd6acd550f3aa496
kybex11 <artm.22@internet.ru>
2024-03-19 08:59:10 +0100

    Added view-container and buttons-container

--- test/app.vue
+++ test/app.vue
@@ -1,5 +1,22 @@
 <template>
-  <FilledButton/>
-  <br>
-  <TintedButton/>
+  <div class="view-container">
+    <h1>Buttons</h1>
+    <div class="buttons-container">
+      <FilledButton/>
+      <br>
+      <TintedButton/>
+    </div>
+  </div>
 </template>
+
+<style>
+.view-container {
+  align-items: center;
+  text-align: center;
+}
+
+.view-container h1 {
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  transform: translateX(-5.5%);
+}
+</style>
